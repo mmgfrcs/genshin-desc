66f16390363410a142d04c76357e2cafe0543b31
Virya Dharmawan <viryadharmawan@gmail.com>
2023-05-07 17:49:01 +0700

    Chore: Change Model

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,8 +1,21 @@
-import {GenshinElement, GenshinPassiveType, GenshinTalentType, GenshinWeapon} from "~/src/models/util";
+import {GenshinElement, GenshinPassiveType, GenshinTalentType, GenshinWeapon} from "./util";
 
+/**
+ * Genshin Impact's Character definition
+ */
 export interface Character {
+  /**
+   * Character's unique ID. Cannot contain spaces
+   * @pattern ^[a-z][a-z0-9\-]*
+   */
   id: string,
+  /**
+   * Character Name
+   */
   name: string,
+  /**
+   * Character Icon to be displayed in the list, alongside name, rarity, weapon and element
+   */
   icon: string,
   portrait: string,
   rarity: number,
@@ -19,8 +32,9 @@ export interface CharacterTalent {
   icon: string,
   name: string,
   images: string[],
-  description: GenshinDescription[]
-  originalDescription: GenshinDescription[]
+  stats: GenshinStat[],
+  description: GenshinDescription[],
+  originalDescription: GenshinDescription[],
   notes: GenshinDescription[]
 }
 
@@ -28,12 +42,18 @@ export interface CharacterPassive {
   type: GenshinPassiveType,
   icon: string,
   name: string,
+  stats: GenshinStat[],
   description: GenshinDescription[]
   originalDescription: GenshinDescription[]
   notes: GenshinDescription[]
 }
 
 export interface CharacterConstellation {
+  /**
+   * Constellation level
+   * @minimum 1
+   * @maximum 6
+   */
   level: number,
   icon: string,
   name: string,
@@ -48,5 +68,11 @@ export interface CharacterConstellation {
 
 export interface GenshinDescription {
   text: string,
-  style?: "subtitle"
+  style?: "subtitle" | "paragraph"
 }
+
+export interface GenshinStat {
+  key: string
+  values: string[]
+}
+
